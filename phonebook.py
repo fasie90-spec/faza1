10d4f3918a038cb4f290d0b2a9f7a5f6f4739359
Ciprian <fasie90@gmail.com>
2026-03-08 19:29:16 +0200

    complete phonebook CLI - set duplicate check, alphabetical sort, JSON save/load

---
+++ phonebook.py
@@ -0,0 +1,77 @@
+# Phonebook CLI
+import json
+
+contacts = {}
+
+def add_contact(name, phone):
+    contacts[name] = phone
+
+def delete_contact(name):
+    contacts.pop(name)
+
+def search_contact(name):
+    print(f"Name: {name} | Phone: {contacts[name]}")
+
+def show_all():
+    if not contacts:
+        print("Your contacts list is empty!")
+        return
+    for i, (name, phone) in enumerate(contacts.items(), start=1):
+        print(f"{i}. Name: {name} | Phone: {phone}")
+
+def menu():
+    load_from_json()
+    while True:
+        raspuns = input("What do you want me to do?")
+
+        if raspuns == "1":
+            name_input = input("What's the name of the person to add?")
+            if name_input in contacts:
+                print("You already have a contact with this name!")
+                continue
+            phone_input = input("What's the number of the person?")
+            number_exists = False
+            for name, phone in contacts.items():
+                if phone == phone_input:
+                    print(f"The number {phone} already exists as {name}!")
+                    number_exists = True
+                    break
+            if number_exists:
+                continue
+            add_contact(name_input, phone_input)
+            print(f"You've added {name_input} to your contacts list!")
+            save_to_json()
+        elif raspuns == "2":
+            delete_input = input("What's the name of the contact you want to delete?")
+            if delete_input not in contacts:
+                print("You dont have this contact registred!")
+                continue
+            delete_contact(delete_input)
+            print(f"You deleted {delete_input} from your contacts list!")
+            save_to_json()
+        elif raspuns == "3":
+            search_input = input("What contact are you looking for?")
+            if search_input not in contacts:
+                print("You dont have this contact registred!")
+                continue
+            search_contact(search_input)
+        elif raspuns == "4":
+            show_all()
+
+def save_to_json():
+    with open("contacts.json", "w") as f:
+        json.dump(contacts, f, indent=2)
+    print("Contacts saved!")
+
+def load_from_json():
+    global contacts
+    try:
+        with open("contacts.json", "r") as f:
+            contacts = json.load(f)
+        print("Contacts loaded!")
+    except FileNotFoundError:
+        print("No saved contacts found.")
+
+
+if __name__ == "__main__":
+    menu()
